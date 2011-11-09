38289a2a0a76887f4a41fe2d999df55f893dbd3a
Nandor Gaspar <gaspar.nandor@mailbox.hu>
2011-11-09 14:09:59 +0100

    SMV result checker added, SMV fix needed

---
+++ check_result.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import numpy, numpy.linalg as la
+
+#--- SMV result checker ----------------------------------->>>
+
+def check_SMV(s, m, v, print_it):
+    m_ = m.reshape(v.size, m.size/v.size)
+    v_ =  v.reshape(v.size, 1)
+
+    if print_it:
+        print "[m:]"+v.size*5*"-"; print m_
+        print "[v:]"+v.size*5*"-"; print v
+    
+        print "[R:]"+v.size*8*"-"
+    
+    return numpy.dot(m_,v)
+
+#--- end block: "SMV result checker" ----------------------<<<
